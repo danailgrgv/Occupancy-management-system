e221ac1cd3e000288a121ae223ed3fb7afd16a32
Georgiev,Danail D.G <d.georgiev@student.fontys.nl>
2021-05-30 20:33:26 +0000

    Update main.cpp

--- network/main.cpp
+++ network/main.cpp
@@ -3,18 +3,8 @@
 #define   MESH_PREFIX     "whateverYouLike"
 #define   MESH_PASSWORD   "somethingSneaky"
 #define   MESH_PORT       5555
-#define BUTTON  0
-#define BUTTONI  22
 int value=0;
-unsigned long lastdebouncetime = 0;
-int reading;
-int lastbuttonstate = 0;
-int debouncetime = 50;
-int buttonstate;
-unsigned long lastdebouncetime1 = 0;
-int reading1;
-int lastbuttonstate1 = 0;
-int buttonstate1;
+
 Scheduler userScheduler; // to control your personal task
 painlessMesh  mesh;
 
@@ -22,53 +12,16 @@ painlessMesh  mesh;
 void idle() ; // Prototype so PlatformIO doesn't complain
 
 Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &idle );
-int read_Button ()
+
+int onGateReceive()
 {
-  int buttonState = digitalRead(BUTTON);
-  if(buttonState != lastbuttonstate)
+  // read I2C
+  uint8_t msg = 0;
+  if(Serial.available() > 0)
   {
-    lastdebouncetime = millis();
+    Serial.read(msg);
   }
-  if(millis() - lastdebouncetime > debouncetime)
-  {
-    if (buttonState != reading)
-    {
-      reading = buttonState;
-      if(buttonState == LOW)
-      {
-        return 1;
-      }
-      else 
-      {
-        return 0;
-      }
-    }
-  }
-  lastbuttonstate = buttonState;
-}
-int read_ButtonI ()
-{
-  int buttonState1 = digitalRead(BUTTONI);
-  if(buttonState1 != lastbuttonstate1)
-  {
-    lastdebouncetime1 = millis();
-  }
-  if(millis() - lastdebouncetime1 > debouncetime)
-  {
-    if (buttonState1 != reading1)
-    {
-      reading1 = buttonState1;
-      if(buttonState1 == LOW)
-      {
-        return 1;
-      }
-      else 
-      {
-        return 0;
-      }
-    }
-  }
-  lastbuttonstate1 = buttonState1;
+  return msg;
 }
 
 // Broadcast Message to increment the Nr of people
@@ -94,8 +47,9 @@ void decrementMeshMessage() {
   mesh.sendBroadcast(str);
 }
 // Send message to destination server for help
-void helpMeMessage(uint32_t dest){
-    DynamicJsonDocument jsonBuffer(1024);
+void helpMeMessage(uint32_t dest)
+{
+  DynamicJsonDocument jsonBuffer(1024);
   JsonObject msg = jsonBuffer.to<JsonObject>();
   msg["type"]=2;
   msg["data"]=1;
@@ -104,8 +58,9 @@ void helpMeMessage(uint32_t dest){
   mesh.sendSingle(dest,str);
 }
 // Broadcast queue state 0 for none 1 for existing queue
-void sendQueue(int state){
-    DynamicJsonDocument jsonBuffer(1024);
+void sendQueue(int state)
+{
+  DynamicJsonDocument jsonBuffer(1024);
   JsonObject msg = jsonBuffer.to<JsonObject>();
   msg["type"]=3;
   msg["data"]=state;
@@ -114,7 +69,8 @@ void sendQueue(int state){
   mesh.sendBroadcast(str);
 }
 // Broadcast the Nr of people in
-void reCheckData(){
+void reCheckData()
+{
   DynamicJsonDocument jsonBuffer(1024);
   JsonObject msg = jsonBuffer.to<JsonObject>();
   msg["type"]=4;
@@ -123,37 +79,89 @@ void reCheckData(){
   serializeJson(msg, str);
   mesh.sendBroadcast(str);
 }
-void idle(){
-if(read_Button()){
-  decrementMeshMessage();
-  value--;
-}
-else if (read_ButtonI()){
-  incrementMeshMessage();
-  value++;
-}
+void idle()
+{
+  int handle = onGateReceive();
+  switch(handle)
+  {
+    case 1:
+    {
+      incrementMeshMessage();
+      value ++;
+      break;
+    }
+    case 2:
+    {
+      decrementMeshMessage();
+      value--;
+      break;
+    }
+    case 3:
+    {
+      Serial.println("Help");
+      break;
+    }
+    case 4:
+    {
+      Serial.println("Queue");
+      sendQueue(1);
+      break;
+    }
+    case 5:
+    {
+      Serial.println("No Queue");
+      sendQueue(0);
+      break;
+    }
+    default:
+    {
+      break;
+    }
+  }
 }
 // Needed for painless library
-void receivedCallback( uint32_t from, String &msg ) {
+void receivedCallback( uint32_t from, String &msg )
+{
   Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-DynamicJsonDocument doc(1024);
-deserializeJson(doc, msg);
-int type=doc["type"];
-int data=doc["data"];
-if (type==1)
-{
-  value=value+data;
-  Serial.print("Value: ");
-  Serial.print(value);
-  Serial.println();
-}
-else 
-{
-  Serial.println("Wrong type");
-}
-
-
-
+  DynamicJsonDocument doc(1024);
+  deserializeJson(doc, msg);
+  int type=doc["type"];
+  int data=doc["data"];
+  switch(type)
+  {
+    case 1:
+    {
+      value = value + data;
+      Serial.print("Value: ");
+      Serial.print(value);
+      Serial.println();
+      break;
+    }
+    case 2:
+    {
+      Serial.println("Help");
+      break;
+    }
+    case 3:
+    {
+      if(data)
+      {
+        Serial.print("Queue on gate ");
+        Serial.println(from);
+      }
+      else
+      {
+        Serial.print("No queue on gate ");
+        Serial.println(from);
+      }
+      break;
+    }
+    default:
+    {
+      Serial.println("Wrong type");
+      break;
+    }
+  }
 }
 
 void newConnectionCallback(uint32_t nodeId) {
@@ -170,8 +178,6 @@ void nodeTimeAdjustedCallback(int32_t offset) {
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BUTTON,INPUT);
-  pinMode(BUTTONI,INPUT);
 
 //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
   mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
